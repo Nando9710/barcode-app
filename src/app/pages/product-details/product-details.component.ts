d21a88bd6b9cf04eeab888bb51592cbe7ab693c1
Fernando Carr <fcarr0397@gmail.com>
2023-11-16 15:32:59 -0500

    feat: product details page

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,12 +1,15 @@
 import { Component, WritableSignal, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../core/interfaces/barcode-products.interface';
+import { MinPricePipe } from '../../core/pipes/min-price.pipe';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MinPricePipe, MatButtonModule, MatChipsModule, MatDividerModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
@@ -16,13 +19,19 @@ export class ProductDetailsComponent {
   public productData: WritableSignal<Product | null> = signal(null)
   private getProductData() {
     this.activatedRoute.queryParams.subscribe({
-      next: (productData) => {
-        console.log(productData);
+      next: ({ productData }) => {
+        if (productData) {
+          this.productData.set(JSON.parse(productData))
+          console.log(this.productData());
+        }
       }
     })
   }
+
+  public goToAmazon(asin: string | undefined) {
+    if (asin) window.open(`https://www.amazon.com/s?k=${asin}`, '_blank');
+  }
   ngOnInit(): void {
     this.getProductData()
-    console.log('holaaaaaa');
   }
 }
