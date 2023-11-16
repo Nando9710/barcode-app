5ce46631afebce6cff2570cd13efce4e6795b07e
Fernando Carr <fcarr0397@gmail.com>
2023-11-16 17:43:00 -0500

    feat: swiper to product details

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,15 +1,30 @@
-import { Component, WritableSignal, signal } from '@angular/core';
+import { Component, ViewChild, WritableSignal, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../core/interfaces/barcode-products.interface';
 import { MinPricePipe } from '../../core/pipes/min-price.pipe';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule, NgFor, NgOptimizedImage } from '@angular/common';
+import { SwiperComponent, SwiperModule } from 'swiper/angular';
+import SwiperCore, { Lazy, Navigation, SwiperOptions } from 'swiper';
+
+
+SwiperCore.use([Navigation, Lazy]);
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [MinPricePipe, MatButtonModule, MatChipsModule, MatDividerModule],
+  imports: [
+    NgFor,
+    CommonModule,
+    MinPricePipe,
+    SwiperModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatDividerModule,
+    NgOptimizedImage
+  ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
@@ -17,6 +32,19 @@ export class ProductDetailsComponent {
   constructor(private activatedRoute: ActivatedRoute) { }
 
   public productData: WritableSignal<Product | null> = signal(null)
+  public config: SwiperOptions = {
+    slidesPerView: 1,
+    speed: 2000,
+    spaceBetween: 0,
+    centeredSlides: true,
+    navigation: true,
+    autoplay: {
+      delay: 10000
+    },
+    pagination: { clickable: true }
+  };
+
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
   private getProductData() {
     this.activatedRoute.queryParams.subscribe({
       next: ({ productData }) => {
