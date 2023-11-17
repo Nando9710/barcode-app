36aac32f4a31750341d96a427eaa6a327230347d
Fernando Carr <fcarr0397@gmail.com>
2023-11-17 16:06:59 -0500

    refactor: global RequestBarcodeApiService app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { RequestBarcodeApiService } from './core/services/request-barcode-api/request-barcode-api.service';
+import { ToastrModule } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
     importProvidersFrom(NgxSkeletonLoaderModule.forRoot({ animation: 'pulse' })),
+    importProvidersFrom(ToastrModule.forRoot()),
     provideAnimations(),
+    RequestBarcodeApiService
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
-import { RequestBarcodeApiService } from './core/services/request-barcode-api/request-barcode-api.service';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
-    providers: [RequestBarcodeApiService]
   },
   {
     path: 'product-details',
     loadComponent: () => import('./pages/product-details/product-details.component').then(c => c.ProductDetailsComponent),
-    providers: [RequestBarcodeApiService]
   },
   {
     path: '',
