28f011866a4f75fad9149f310b357958f2ff228c
Fernando Carr <fcarr0397@gmail.com>
2023-11-21 19:23:28 -0500

    test: test google-product-taxonomy

--- src/app/core/services/google-product-taxonomy/google-product-taxonomy.service.spec.ts
+++ src/app/core/services/google-product-taxonomy/google-product-taxonomy.service.spec.ts
@@ -1,16 +1,50 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GoogleProductTaxonomyService } from './google-product-taxonomy.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('GoogleProductTaxonomyService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let service: GoogleProductTaxonomyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GoogleProductTaxonomyService);
+    TestBed.configureTestingModule({ providers: [HttpClient, HttpHandler] });
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new GoogleProductTaxonomyService(httpClientSpy);
+    // service = TestBed.inject(GoogleProductTaxonomyService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getGoogleProductTaxonomyData() should return an observable string[]', (done: DoneFn) => {
+    const expectedReturnedData: string[] = [
+      "Furniture > Baby & Toddler Furniture > Baby & Toddler Furniture Sets",
+      "Furniture > Baby & Toddler Furniture > Bassinet & Cradle Accessories"
+    ];
+
+    httpClientSpy.get.and.returnValue(of(expectedReturnedData));
+
+    service.getGoogleProductTaxonomyData().subscribe({
+      next: (values) => {
+        expect(values).withContext('expected values').toEqual(expectedReturnedData);
+        done()
+      }
+    })
+
+    expect(httpClientSpy.get.calls.count()).withContext('one call').toBe(1);
+  })
+
+  it('setProductsData(data: Product[]) should set products data', () => {
+    const data: string[] = [
+      "Furniture > Baby & Toddler Furniture > Baby & Toddler Furniture Sets",
+      "Furniture > Baby & Toddler Furniture > Bassinet & Cradle Accessories"
+    ];
+    service.setProductTaxonomyData(data);
+    service.productTaxonomyData$.subscribe((productsValue) => {
+      expect(productsValue).toEqual(data);
+    });
   });
 });
